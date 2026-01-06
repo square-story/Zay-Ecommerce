14355927cca3e5037e46bd87b7a82835b927b7c8
SADIK <gibmepreo@gmail.com>
2026-01-06 20:13:11 +0530

    fix: update coupon limit check to allow usage when limit is 1 or more

--- controller/cartCotroller.js
+++ controller/cartCotroller.js
@@ -272,7 +272,7 @@ class CartController {
         // Check if the coupon has not been used by the current user
         const hasNotBeenUsedByUser = !coupon.userUsed.includes(userId);
 
-        const haveLimit = coupon.limit > 1;
+        const haveLimit = coupon.limit >= 1;
 
         return isActive && meetsMinOrderValue && hasNotBeenUsedByUser && haveLimit;
       });
